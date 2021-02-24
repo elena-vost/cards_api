c658793d1ec743c04f2b7dc0d62c4c159d4f6715
elena-vost <angara1298@gmail.com>
2021-02-24 12:26:03 +0100

    button added

--- src/Deck.js
+++ src/Deck.js
@@ -7,15 +7,24 @@ class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = { deck: null };
+    this.getCard = this.getCard.bind(this);
   }
   async componentDidMount() {
-    let deck = await axios.get(API_BASE_URL);
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
     this.setState({ deck: deck.data });
+  }
+  async getCard() {
+    let id = this.state.deck.deck_id;
+    let cardUrl = `${API_BASE_URL}/${id}/draw`;
+    let cardRes = await axios.get(
+      "http://deckofcardsapi.com/api/deck/beo4xyn81fko/draw/"
+    );
   }
   render() {
     return (
       <div>
         <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
       </div>
     );
   }
