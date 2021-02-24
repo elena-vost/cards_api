de373ad00d57d8d1ed7be8b8dbb3077f1008bccf
elena-vost <angara1298@gmail.com>
2021-02-24 12:03:30 +0100

    axios installed git add .

--- src/Deck.js
+++ src/Deck.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
+const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = { deck: null };
   }
-
+  componentDidMount() {
+    axois;
+  }
   render() {
     return (
       <div>
