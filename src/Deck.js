a787597ea090c6ab8a0502c5e7ed373afab6a49d
elena-vost <angara1298@gmail.com>
2021-02-24 12:00:05 +0100

    new const

--- src/Deck.js
+++ src/Deck.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deck: null };
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+      </div>
+    );
   }
 }
 
