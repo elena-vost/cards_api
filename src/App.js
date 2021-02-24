53ba052eb13b4a54de4bb18a70fff15733702959
elena-vost <angara1298@gmail.com>
2021-02-24 12:16:12 +0100

    fixed url '

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Deck from "./Deck";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Deck />
+    </div>
+  );
 }
 
 export default App;
--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle";
+
+import axios from "axios";
+const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = { deck: null };
   }
-  componentDidMount() {
-    axois;
+  async componentDidMount() {
+    let deck = await axios.get(API_BASE_URL);
+    this.setState({ deck: deck.data });
   }
   render() {
     return (
